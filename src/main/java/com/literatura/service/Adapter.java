1ee5a12c720e3c3916cb1ddc8026c83b4dd3027b
CZrich <czrichardz@gmail.com>
2024-11-30 17:36:54 -0500

    add connection to gutendex api

---
+++ src/main/java/com/literatura/service/Adapter.java
@@ -0,0 +1,20 @@
+package com.literatura.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class Adapter  implements IAdapter{
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    public <T> T getDataFromJson(String json, Class<T> clase) {
+        try {
+
+            return objectMapper.readValue(json,clase);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
